f1b2feff43a8c2a9e82e74a0081a007e35da831f
shashwat gupta <shashwatgupta4656@gmail.com>
2021-01-12 22:39:21 +0530

    get all bootcamps

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -5,8 +5,13 @@ const Bootcamp = require('../models/Bootcamps');
 // @desc    get all Bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'get all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false, msg: `${err.message}` });
+  }
 };
 
 // @desc    get single Bootcamps
