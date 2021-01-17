a7d547004270f9a2901cebaa2925b2a001826e9f
shashwat gupta <shashwatgupta4656@gmail.com>
2021-01-17 18:38:21 +0530

    add slugs in bootcamps

--- models/Bootcamps.js
+++ models/Bootcamps.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -98,6 +99,12 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// slugifying the name so that our front end can use it to route
+BootcampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
