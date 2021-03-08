c35733fe1e9ca93b9892a6f5cf396eb1ba0c5b80
shashwat gupta <shashwatgupta4656@gmail.com>
2021-03-09 02:16:08 +0530

    select and sort in get all bootcamps

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -3,12 +3,50 @@ const router = express.Router();
 const Bootcamp = require('../models/Bootcamps');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 
 // @desc    get all Bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.find();
+  let query;
+
+  // Create copy of req.query
+  const reqQuery = { ...req.query };
+
+  // Feilds to exclude
+  const removeFeilds = ['select', 'sort'];
+
+  // Loop over removeFeilds and exclude the from reqQuery
+  removeFeilds.forEach((param) => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create Operators like $gt etc
+  queryStr = queryStr.replace(
+    /\b'gt|gte|lt|lte|in'\b/g,
+    (match) => `$${match}`
+  );
+
+  // Finding resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select Feild
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  const bootcamp = await query;
   res
     .status(200)
     .json({ success: true, count: bootcamp.length, data: bootcamp });
@@ -56,4 +94,30 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     return next(err);
   }
   res.status(200).json({ success: true, data: bootcamp });
+});
+
+// @desc    Get bootcamps within a radius
+// @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Public
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get longitude/ latitude from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // calculate radius in radians
+  // Divide by earth radius
+  // 6378 in km / 3963 in miles
+
+  const radius = distance / 3963;
+
+  const bootcamp = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res
+    .status(200)
+    .json({ success: true, count: bootcamp.length, data: bootcamp });
 });
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,7 +7,10 @@ const {
   createBootcamp,
   editBootcamp,
   deleteBootcamp,
+  getBootcampsInRadius,
 } = require('../controller/bootcamps');
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router
   .route('/')
